afd6098571d34f1ece63d74e67af7c5c4f22201c
Cecil Urena <Cecil.Urena@kasasa.com>
2017-02-18 16:59:35 -0600

    Added app role policy to enable app to read/write secrets. Devised a mechanism to feed the secrets to Vault while in localdev.

--- src/springboot.sh
+++ src/springboot.sh
@@ -27,10 +27,23 @@ sb::up() {
 		/bin/sh -c 'apk -v --update --no-cache add jq; \
 					export VAULT_ADDR=http://$VAULT_PORT_8200_TCP_ADDR:8200; \
 					vault auth-enable approle; \
-					vault write auth/approle/role/$APP_NAME bind_secret_id=true; \
+					echo '\''{"path":{"secret/$APP_NAME":{"policy":"write"}}}'\'' | \
+					vault policy-write $APP_NAME -; \
+					vault write auth/approle/role/$APP_NAME bind_secret_id=true token_ttl=5m token_max_ttl=10m policies=$APP_NAME; \
 					vault read -format=json auth/approle/role/$APP_NAME/role-id | jq .data.role_id > roleId.txt; \
 					vault write -f -format=json auth/approle/role/$APP_NAME/secret-id | jq .data.secret_id > secretId.txt; \
 					mv *.txt /mnt/creds;' \
+		|| return 1
+
+	docker run -it \
+		--rm \
+		--link vault:vault \
+		-v `pwd`/.vault/secrets:/mnt/secrets \
+		-e VAULT_TOKEN=horde \
+		-e APP_NAME="${name}" \
+		vault:0.6.5  \
+		/bin/sh -c 'export VAULT_ADDR=http://$VAULT_PORT_8200_TCP_ADDR:8200; \
+					vault write secret/$APP_NAME `cat /mnt/secrets/secrets.txt`' \
 		|| return 1
 
 	docker run -d \
