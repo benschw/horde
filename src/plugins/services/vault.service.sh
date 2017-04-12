eda6ac4d1630972d6c2d2525ce951f84b60c53ae
Cecil Urena <Cecil.Urena@kasasa.com>
2017-04-11 19:05:27 -0500

    Removed function that creates app role from vault service.

--- src/plugins/services/vault.service.sh
+++ src/plugins/services/vault.service.sh
@@ -43,20 +43,3 @@ services::vault() {
 		    VAULT_ADDR=http://$VAULT_PORT_8200_TCP_ADDR:8200 vault policy-write admins -' \
 		|| return 1
 }
-
-services::vault::create_app_role() {
-    local name="$1"
-
-    docker run -it \
-		--rm \
-		--link vault:vault \
-		-e VAULT_TOKEN=horde \
-		-e APP_NAME="${name}" \
-		vault:0.6.5  \
-		/bin/sh -c 'export VAULT_ADDR=http://$VAULT_PORT_8200_TCP_ADDR:8200; \
-					vault auth-enable approle; \
-					echo '\''{"path":{"secret/'\''$APP_NAME'\''":{"policy":"write"}}}'\'' | \
-					vault policy-write $APP_NAME -; \
-					vault write auth/approle/role/$APP_NAME bind_secret_id=true token_ttl=5m token_max_ttl=10m policies=$APP_NAME;' \
-		|| return 1
-}
