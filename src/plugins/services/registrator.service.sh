988f16f662ae44f0e07814c1ee20c139a06b1d23
Ben Schwartz <benschw@gmail.com>
2017-07-17 10:14:59 -0500

    updating docs

---
+++ src/plugins/services/registrator.service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+services::registrator() {
+	local ip=$(net::bridge_ip)
+
+	service::ensure_running consul || return 1
+
+	container::call run \
+		-d \
+		--name=registrator \
+		--net=host \
+		--volume=/var/run/docker.sock:/tmp/docker.sock \
+		gliderlabs/registrator:latest \
+		-ip $ip consul://localhost:8500 || return 1
+}
+
+
