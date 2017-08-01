8010efc66fa03d6b73476588f4c76bbafe98afd5
Ben Schwartz <benschw@gmail.com>
2017-08-01 12:04:03 -0500

    updating mysql service to use core container

--- src/contrib/services/mysql.service.sh
+++ src/contrib/services/mysql.service.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 services::mysql() {
 	local ip=$(net::bridge_ip)
 	local name="mysql"
@@ -13,29 +11,45 @@ services::mysql() {
 		port_cfg="${HORDE_MYSQL_PUBLISH_PORT}:3306"
 	fi
 
-        if [[ $HORDE_MYSQL_PERSIST_DATA ]]; then
-                container::call volume create --name mysql-data
-                local mysql_volume_arg="-v mysql-data:/var/lib/mysql"
-        fi
-
-	container::call run \
-		-d \
-		-p $port_cfg \
+	container::call run -d \
+		-p $port_cfg $mysql_volume_arg \
 		-e "SERVICE_3306_NAME=${name}" \
-		$mysql_volume_arg \
+		-e "MYSQL_ALLOW_EMPTY_PASSWORD=yes" \
 		--name $name \
 		--dns $ip \
-		benschw/horde-mysql || return 1
+		mysql:5.7 || return 1
 
-	sleep 5
+	echo "Waiting for MySQL to start up"
+	ctr=$((25))
+	secs=$ctr
+	while [ $secs -gt 0 ]; do
+
+		left=$secs
+		while [ $left -gt 0 ]; do
+			echo -n "."
+			: $((left--))
+		done
+		left=$secs
+		while [ $left -lt $ctr ]; do
+			echo -n ' '
+			: $((left++))
+		done
+		echo -ne "\r"
+		sleep 1
+		: $((secs--))
+	done
+	
+	container::call run -it --rm --link mysql:mysql mysql:5.7 \
+		sh -c 'exec mysql -h$MYSQL_PORT_3306_TCP_ADDR -u root -e "GRANT ALL ON *.* TO admin@'\''%'\'' IDENTIFIED BY '\''changeme'\'' WITH GRANT OPTION; FLUSH PRIVILEGES"'
 }
+
 
 services::mysql::create_database() {
 	db_name="${1//-/_}"
 
 	container::call run -it --rm \
 		--link mysql:mysql \
-		benschw/horde-mysql \
+		mysql:5.7 \
 		sh -c 'exec mysql -h$MYSQL_PORT_3306_TCP_ADDR -u admin -pchangeme -e "CREATE DATABASE IF NOT EXISTS '$db_name'"'
 
 }
