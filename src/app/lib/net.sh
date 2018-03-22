9e5a84ceb683d62b7d766f5e88e3dbe9c19a0906
Karl Tiedt <ktiedt@gmail.com>
2018-03-22 15:14:32 -0500

    Fixes #42 - slice hostname down to just hostname even if it contains a /urlfragment

--- src/app/lib/net.sh
+++ src/app/lib/net.sh
@@ -19,15 +19,15 @@ net::configure_hosts() {
 	local ip=$(net::bridge_ip)
 
 	for host in "${hosts[@]}"; do
-          if ! hostess has $host > /dev/null ; then
+					hostonly=$(echo $host | awk -F/ '{print $1}')
+          if ! hostess has $hostonly > /dev/null ; then
                 if [ ! -z "$HORDE_DEBUG" ]; then
-                        io::err "sudo hostess add $host $ip"
+                        io::err "sudo hostess add $hostonly $ip"
                 fi
-                if ! sudo hostess add $host $ip > /dev/null ; then
+                if ! sudo hostess add $hostonly $ip > /dev/null ; then
                         io::err "problem configuring hostname '${host}'"
                         return 1
                 fi
           fi
         done
 }
-
